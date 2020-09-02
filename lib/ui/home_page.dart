4dccc8344e405ca7884d5c44aa423b35b19254a0
Tais Costa <taiscguerra@gmail.com>
2020-09-02 17:56:42 -0300

    grid de gifs adicionado

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:transparent_image/transparent_image.dart';
 import 'package:share/share.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -42,7 +41,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: PreferredSize(
           child: AppBar(
@@ -57,21 +55,18 @@ class _HomePageState extends State<HomePage> {
           ),
           preferredSize: Size.fromHeight(100.0)),
       backgroundColor: Color(0xFF121212),
-      body: Column(
-        children: <Widget>[
+      body: Column(children: <Widget>[
         Padding(
-          padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
           child: TextField(
             maxLines: 1,
-            maxLength: 30,
             cursorColor: Color(0xFF6F6B6B),
             controller: _searchController,
             decoration: InputDecoration(
-              
               prefixIcon: Icon(
                 Icons.search,
                 color: Color(0xFF3FDB90),
-                ),
+              ),
               hintText: "Search GIPHY",
               hintStyle: TextStyle(
                 color: Color(0xFF6F6B6B),
@@ -84,7 +79,7 @@ class _HomePageState extends State<HomePage> {
                   style: BorderStyle.none,
                 ),
               ),
-              focusedBorder:  OutlineInputBorder(
+              focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(100.0)),
                 borderSide: BorderSide(
                   width: 0.0,
@@ -92,18 +87,61 @@ class _HomePageState extends State<HomePage> {
                   style: BorderStyle.none,
                 ),
               ),
-              
               contentPadding: EdgeInsets.all(8.0),
               fillColor: Color(0xFF333232),
               filled: true,
             ),
-            
             style: TextStyle(
               color: Color(0xFFCAC7C7),
             ),
           ),
-          ),
+        ),
+        Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: RefreshProgressIndicator(
+                          backgroundColor: Color(0xFF333232),
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Color(0xFF3FDB90)),
+                          strokeWidth: 2.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError)
+                        return Container();
+                      else 
+                        return _createGifTable(context, snapshot);
+                  }
+                }))
       ]),
     );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0
+        ), 
+      itemCount: snapshot.data["data"].length,  
+      itemBuilder: (context, index){
+        return GestureDetector(
+          child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+          height: 300.0,
+          fit: BoxFit.cover,
+          ),
+            
+        );
+      }
+      );
   }
 }
